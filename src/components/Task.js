07a297fe1b1a342eb8fa3c286544831afb7dee97
Milen Urkov <milenski1989@abv.bg>
2022-01-17 18:01:19 +0200

    Undo "Complete tasks" functionality

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,17 +4,13 @@ import './Task.css'
 
 const Task = ({task, index, handleRemoveItem}) => {
 
-	const [checked, setChecked] = useState(false)
 
-	function changeCheckBox (event){
-		setChecked(event.target.checked)
 	
-	}
 
 	return <div className="Task" key={index}> 
 		  <div>{task.title}</div>
 		  <button onClick={() => handleRemoveItem(index)}>remove</button>
-		  <input type="checkbox" checked={checked} onChange={changeCheckBox}></input>
+		  <input type="checkbox"></input>
 	 </div>
 }
 
