dc1cf99f858ede10a94a7381e44a62af0f30d2cc
Milen Urkov <milenski1989@abv.bg>
2022-01-17 13:07:16 +0200

    Eslint config + auto navigating users to login page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Tasks from './Tasks';
-import Profile from './Profile';
+import logo from "./logo.svg"
+import "./App.css"
+import { Route, Routes } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import Tasks from "./components/Tasks"
+import Profile from "./components/Profile"
+import Login from "./components/Login"
+import { useEffect, useState } from "react"
 
 function App() {
-  return (
-    <Routes>
-        <Route path="/" element={<Tasks />} />
-        <Route path="profile" element={<Profile />} />
-    </Routes>
-  );
+	let navigate = useNavigate()
+	useEffect(() => {
+		navigate("login", { replace: true })
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Tasks />} />
+				<Route path="profile" element={<Profile />} />
+				<Route path="login" element={<Login />} />
+			</Routes>
+		</>
+	)
 }
 
-export default App;
+export default App
