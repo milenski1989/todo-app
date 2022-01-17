8ff722d55236edaeba5f6e99730d7b85f39045e2
Milen Urkov <milenski1989@abv.bg>
2022-01-17 17:57:49 +0200

    "Complete tasks" functionality - done

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.development'
 import './Task.css'
 
 const Task = ({task, index, handleRemoveItem}) => {
+
+	const [checked, setChecked] = useState(false)
+
+	function changeCheckBox (event){
+		setChecked(event.target.checked)
+	
+	}
+
 	return <div className="Task" key={index}> 
 		  <div>{task.title}</div>
 		  <button onClick={() => handleRemoveItem(index)}>remove</button>
-		  <input type="checkbox"></input>
+		  <input type="checkbox" checked={checked} onChange={changeCheckBox}></input>
 	 </div>
 }
 
