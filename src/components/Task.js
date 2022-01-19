912059617932c13d83c186bff21ea3c0f26610a2
Milen Urkov <milenski1989@gmail.com>
2022-01-19 16:04:14 +0200

    New tasks - float to left + react modal install

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from "react"
 import fakeServer from "../FakeServer"
 import Tasks from "./Tasks"
+import Modal from 'react-modal'
 
 
 function Task({ task, index, handleRemoveItem, check }) {
 
-    return (<div className="task-container">
-        <div className="new-task" key={index}>
-            <input type="checkbox" checked={task.completed} onChange={() => check(index)}></input>
-            <div className={task.completed ? "task-completed" : "task-notcompleted"}>
-                {task.title}</div>
-            <button onClick={() => handleRemoveItem(index)}>remove</button>
+    return (<div className="multiple-tasks-container">
+        <div className="single-task-container">
+            <div className="new-task" key={index}>
+                <input type="checkbox" checked={task.completed} onChange={() => check(index)}></input>
+                <div className={task.completed ? "task-completed" : "task-notcompleted"}>
+                    {task.title}</div>
+                <button onClick={() => handleRemoveItem(index)}>remove</button>
+            </div>
         </div>
     </div>
+
     )
 
 
